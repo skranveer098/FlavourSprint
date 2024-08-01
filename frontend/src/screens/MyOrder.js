739d010f1fb0c716b2750b2c3a7682a82b6a2baa
ranveer <ranveersinghsingh2644@gmail.com>
2024-08-01 12:36:04 +0530

    Link added

--- frontend/src/screens/MyOrder.js
+++ frontend/src/screens/MyOrder.js
@@ -8,7 +8,7 @@ export default function MyOrder() {
 
     const fetchMyOrder = async () => {
         console.log(localStorage.getItem('userEmail'));
-        await fetch("http://localhost:5000/api/auth/myOrderData", {
+        await fetch("https://flavour-sprint.vercel.app/api/auth/myOrderData", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
